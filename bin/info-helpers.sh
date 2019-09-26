759efb63581ecc2e87017a025b8c12445657af56
Fernando Basso <fernandobasso.br@gmail.com>
2019-09-26 16:52:11 -0300

    info helper shell script

---
+++ bin/info-helpers.sh
@@ -0,0 +1,70 @@
+
+function usage () {
+    script_name=${0##*/}
+
+    cat << EOF
+
+  Lists the menus of a program which provides info pages.
+
+  USAGE
+
+    $script_name [OPTION]... PROGRAM
+
+  EXAMPLES
+
+    $script_name sed
+    $script_name emacs
+    $script_name find
+EOF
+
+    exit 1
+}
+
+
+if [ -z $1 ] || [ -z $2 ] ; then
+    usage
+fi
+
+
+function titlefy () {
+    title="‘${1}’ info menu index"
+    pad=$(printf '%0.1s' ={1..120})
+    remaining=$(( 80 - "${#title}" ))
+    padlen=$(( $remaining / 2 ))
+    printf '%.*s %s %.*s\n' $padlen $pad "$title" $padlen $pad
+}
+
+
+#
+# Gets a string and makes a line-like string of the length
+# of the passed string.
+#
+function linefy () {
+    printf '%s' "$1" | sed 's/./=/g'
+}
+
+
+function info_parse_index () {
+    printf '%s' "$( info "$1" --output - | grep '^\*\s.\+::' )"
+}
+
+
+function list_menus () {
+    title=$(titlefy "$1")
+
+    printf '\n%s\n' "$( linefy "$title" )"
+    printf '%s\n' "$title"
+    printf '%s\n' "$( linefy "$title" )"
+
+    printf '\n%s\n' "$( info_parse_index "$1" )"
+}
+
+case "$1" in
+    --menus)
+        list_menus "$2"
+        ;;
+    *)
+        echo 'default ...'
+        ;;
+esac
+

d34ae2d555bb1f7124c0906f165af85cfe9be310
Fernando Basso <fernandobasso.br@gmail.com>
2019-12-12 12:58:53 -0300

    tmux script for proghowto session

---
+++ bin/tmux-proghowto.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+function usage () {
+  cat <<EOF
+
+Usage:
+
+  ${0##*/} -, --help
+    Show this help text and exit.
+
+  ${0##*/} [WORKING-DIRECTORY] [SESSION-NAME]
+    Start tmux in WORKING-DIRECTORY with session named SESSION-NAME
+
+EOF
+
+  exit 0
+}
+
+case "$1" in
+  -h|--help)
+    usage
+    ;;
+esac
+
+dir="${1:-~/Projects/proghowto}"
+ses="${2:-proghowto}"
+
+tmux -f ~/Projects/dotfiles/tmux/themes/tmux-light.conf \
+  new-session -d -s "$ses" -c "$dir" \; \
+  rename-window 'vim' \; \
+  send-keys 'vim -c NERDTree -c "colorscheme mylight1" -c "AirlineTheme xtermlight"' C-m
+
+tmux split-window -h -c "$dir" \; split-window -v -c "$dir"
+
+tmux new-window -t "${ses}:2" -n 'git' -c "$dir"
+
+tmux select-window -t "${ses}:1"
+tmux -2 attach-session -t "${ses}"
+
+# vim: set softtabstop=2 shiftwidth=2:
+

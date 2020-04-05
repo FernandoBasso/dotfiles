8c097979e13f21adad9dca81be62e5d414ed70e2
Fernando Basso <fernandobasso.br@gmail.com>
2020-04-05 17:10:47 -0300

    Improve git aware prompt for bash

--- bash_incl/bash-prompts.sh
+++ bash_incl/bash-prompts.sh
@@ -1,3 +1,15 @@
+#
+# Git Prompt
+# ==========
+#
+# https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+#
+
+GIT_PS1_SHOWDIRTYSTATE=1
+GIT_PS1_SHOWSTASHSTATE=1
+GIT_PS1_SHOWUNTRACKEDFILES=1
+GIT_PS1_SHOWUPSTREAM=git
+
 
 # For a terminal emulator includes a ✔ char in the git prompt thing.
 if test "$TERM" == linux ; then
@@ -6,10 +18,8 @@ else
   check=$'\u2714'
 fi
 
-hr='--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'
-# Basic prompt. No git brach thing, no node/ruby version.
-ps1basic () {
-  PS1='\n\[\e[0;34m\]$(printf "%s\n" "${hr:0:${COLUMNS:-$(tput cols)}}")\n\[\e[0;34m\]\w/\[\e[1;0m\]\n\$ '
+curdir () {
+  printf '%s' "${PWD##*/}"
 }
 
 version_git () {
@@ -21,26 +31,30 @@ version_node () {
   printf '%s' "$(node --version)"
 }
 
+hr='--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'
+# Basic prompt. No git brach thing, no node/ruby version.
+ps1basic () {
+  PS1='\n\[\e[0;34m\]$(printf "%s\n" "${hr:0:${COLUMNS:-$(tput cols)}}")\n\[\e[0;34m\]\w/\[\e[1;0m\]\n\$ '
+}
+
 # PS1 with ruler, bash/node/ruby version and git prompt.
 ps1all () {
-  PS1='\n\[\e[0;34m\]$(printf "%s\n" "${hr:0:${COLUMNS:-$(tput cols)}}")\n\[\e[0;35m\][bash-$(echo -n $BASH_VERSION)] [$(~/.rvm/bin/rvm-prompt)] $(echo -n [node-`node -v`)] \e[0m\[\e[0;35m\][$(version_git)] \e[0;31m$(__git_ps1 "[%s ${check}]")\n\[\e[0;34m\]$(date +'%H:%M:%S') \[\e[0;34m\]\w/ \n \[\e[1;0m\] \n\$ '
+  PS1='\n\[\e[0;34m\]$(printf "%s\n" "${hr:0:${COLUMNS:-$(tput cols)}}")\n\[\e[0;35m\][bash-$(echo -n $BASH_VERSION)] [$(~/.rvm/bin/rvm-prompt)] $(printf '%s' [node-`node -v`)] \e[0m\[\e[0;35m\][$(version_git)] \e[0;31m$(__git_ps1 "[%s ${check}]")\n\[\e[0;34m\]$(date +'%H:%M:%S') \[\e[0;34m\]\w/ \n \[\e[1;0m\] \n\$ '
 }
 
 
 ps1simple () {
-  PS1='\n\[\e[0;34m\]\w \e[0;31m$(__git_ps1 "[%s ${check}]")\n\[\e[1;0m\]\n$ '
+  PS1='\n\[\e[0;34m\]\w \e[0;31m$(__git_ps1 "[%s]")\n\[\e[1;0m\]\n$ '
 }
 
 git_info () {
-  printf '%s' "$red$(__git_ps1 "[%s ${check}]")"
+  printf '%s' "$red\$(__git_ps1 "[%s]")"
 }
 
 ps1nodejs () {
-  node="$(version_node)"
-  dir="${PWD##*/}"
-
-  PS1="\n${blue}$dir ${cyan}[node:$node] $(git_info)\n${normal}$ "
+  PS1="\n${blue}\$(curdir) ${cyan}[node:\$(version_node)] $(git_info)\n${normal}$ "
 }
+
 
 #
 # Sets the default prompt.
@@ -48,4 +62,5 @@ ps1nodejs () {
 ps1nodejs
 
 # vim: set filetype=sh softtabstop=2 shiftwidth=2:
+# vim: set nowrap:
 

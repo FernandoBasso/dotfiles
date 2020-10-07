73d61217b2025e8e60f172dfbf215984bb22c113
Fernando Basso <fernandobasso.br@gmail.com>
2020-10-07 18:47:59 -0300

    Racket xp mode colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,7 +24,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(racket-xp-def-face ((t (:inherit match :background "pale goldenrod" :foreground "dark magenta" :underline t))))
+ '(racket-xp-use-face ((t (:inherit match :background "pale goldenrod" :foreground "dark magenta")))))
 
 ;;
 ;; CUSTOM SETTINGS

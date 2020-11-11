ab423d27868e221413f9119d3aa5c73efd54293b
Fernando Basso <fernandobasso.br@gmail.com>
2020-11-11 08:01:19 -0300

    Add script to send keys to tmux windows and panes

---
+++ bin/tmux-send-keys.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+##
+# Helper script to send keys (a command line) to all windows and panes
+# of the current session.
+#
+# Credits where it is due:
+# https://scripter.co/command-to-every-pane-window-session-in-tmux/
+#
+# ⚠️  WARN ⚠️ : If there is a program such vim or emacs running on a pane
+# or window, the keys will be sent to that window to, messing things up.
+# So, make sure you have only shells running on each window or pane
+# before using this script. In the future, I'll try to detect that we
+# only have a shell running on each target before sending keys to them.
+#
+# My use case is basically running `nvm use` (because I have lots of
+# Node, TypeScript and other JS related stuff which run on different
+# versions) or `clear`. Very simple stuff.
+#
+# EXAMPLE USAGE:
+#
+#   $ echo hello
+#   $ nvm use
+#   $ clear
+#
+# 🔥️ DANGER 🔥️: Note that sending commands to multiple shells is
+# inherently dangerous. Terminator, Tilix and possible other programs
+# allow sending commands to multiple running shells and the danger looms
+# with all of them. Imagine if you do `pacman -S <some package> or `git
+# commit -m 'foo' in all available shells‽ So, please DO BE CAREFUL!
+##
+
+tmux list-panes -s -F '#{session_name}:#{window_index}.#{pane_index}' \
+  | xargs -I PANE tmux send-keys -t PANE "$*" C-j
+
+
+# vim: set tw=72:
+

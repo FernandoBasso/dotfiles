4733f19867f26ea555ccce237286ffebb890a40f
Fernando Basso <fernandobasso.br@gmail.com>
2021-05-27 09:50:06 -0300

    emacs: try dired-sidebar instead of neotree

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -251,8 +251,27 @@
 (use-package htmlize
   :ensure t)
 
-(use-package neotree
-  :ensure t)
+(use-package vscode-icon
+  :ensure t
+  :commands (vscode-icon-for-file))
+
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
 
 (global-set-key [f4] 'neotree-toggle)
 (setq neo-window-width 32)

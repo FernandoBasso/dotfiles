1eb03ae4d12f7e9366cd63470f754de926ee1ebd
Fernando Basso <fernandobasso.br@gmail.com>
2021-09-06 13:20:31 -0300

    git: Create some bash aliases for git
    Git built-in alias functionality has some limitations. We still need to
    type ‘git’, a space, then some more chars. It can't be as short as ‘gl’,
    for instance. Also, I couldn't find a way to use custom ENV VARS like
    
    	git config --global alias.gl 'COLUMNS=72 <rest of command>'
    
    The above seems to be unachievable. Thus, let's resort to good old bash
    aliases.

--- bash_incl/bash-aliases.sh
+++ bash_incl/bash-aliases.sh
@@ -23,3 +23,74 @@ alias vimlight='vim -c "colorscheme mylight1" -c "AirlineTheme xtermlight"'
 
 alias fzf=fzf --delimiter=: --preview='bat --style=numbers --color=always {1}'
 
+########################################################################
+# Git related aliases.
+#
+# Git mechanism for aliases is helpful, but it has some shortcomings. For
+# one thing, we just type `git <some command>'. We can't have something
+# quick and simple as `gl' for a customized `git log' or `gs' for some
+# `git status' command. Also, I couldn't find a way to have it work with
+# ENV VARS.
+#
+# So, here are some bash (not git) aliases around some git commands.
+#
+# TODO: I still need to try to figure out if there are other programs
+# whose names would clash with my aliases for git below. Perhaps I could
+# make my aliases always start with an uppercase ‘G’ or something else.
+##
+
+##
+# ====================
+# gci: Git Commit Info
+# ====================
+#
+# Just like a `git log' but with custom options. I use this one most for
+# work related tasks where I want to save my commits to my org-mode
+# notes so I can have a reference in case the repository gets messy or I
+# just need a refresher on something.
+#
+# Usage Examples
+# --------------
+#
+# No limit. We can keep paging:
+#
+# 	$ gci
+#
+# Just the last commit:
+#
+# 	$ gci -1
+#
+alias gci="COLUMNS=78 git log --format=$'\n %s\n %h %an <%ae>' --abbrev=11 --stat"
+
+##
+# gs: Git Status
+#
+# Usage:
+#
+# 	$ gs
+#
+alias gc="git status"
+
+##
+# Git Log
+#
+# Usage:
+#
+# 	$ gl
+# 	$ gl -3
+#
+alias gl="git log"
+
+##
+# Git Log Oneline with Graph
+#
+# Usage:
+#
+# 	$ g1l
+# 	$ g1l -5
+#
+alias g1l="git log --oneline --graph"
+
+#
+# End Git Aliases
+########################################################################

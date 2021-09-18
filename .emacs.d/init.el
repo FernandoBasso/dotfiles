55f2ea133a04abc200860897d67265aec91392d1
Fernando Basso <fernandobasso.br@gmail.com>
2021-09-18 07:33:57 -0300

    emacs: Add typescript-related packages and configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,8 +12,10 @@
  '(custom-safe-themes
    '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "8e797edd9fa9afec181efbfeeebf96aeafbd11b69c4c85fa229bb5b9f7f7e66c" "2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" default))
  '(magit-push-arguments nil)
-  '(package-selected-packages
-   '(emojify helm-org-ql org-ql imenu-list markdown-mode yasnippet helm-rg helm-projectile helm-ag helm heml sublimity-scroll sublimity-attractive sublimity diff-hl git-gutter solarized-theme typescript-mode editorconfig geiser gruvbox-theme projectile neotree paredit haskell-mode org-plus-contrib magit htmlize zenburn-theme color-theme which-key racket-mode use-package))
+ '(org-agenda-files
+   '("/Users/fernando/Sync-pCloud/org-mode/C.org" "/Users/fernando/Sync-pCloud/org-mode/CSS.org" "/Users/fernando/Sync-pCloud/org-mode/algorithms.org" "/Users/fernando/Sync-pCloud/org-mode/anki.org" "/Users/fernando/Sync-pCloud/org-mode/books (Case Conflict (1)).org" "/Users/fernando/Sync-pCloud/org-mode/books (Case Conflict).org" "/Users/fernando/Sync-pCloud/org-mode/books.org" "/Users/fernando/Sync-pCloud/org-mode/captured.org" "/Users/fernando/Sync-pCloud/org-mode/career.org" "/Users/fernando/Sync-pCloud/org-mode/command-line.org" "/Users/fernando/Sync-pCloud/org-mode/common-lisp.org" "/Users/fernando/Sync-pCloud/org-mode/cool-projects.org" "/Users/fernando/Sync-pCloud/org-mode/ctags.org" "/Users/fernando/Sync-pCloud/org-mode/data-modeling.org" "/Users/fernando/Sync-pCloud/org-mode/databases.org" "/Users/fernando/Sync-pCloud/org-mode/deskenv-winmanagers.org" "/Users/fernando/Sync-pCloud/org-mode/docker.org" "/Users/fernando/Sync-pCloud/org-mode/documentaries.org" "/Users/fernando/Sync-pCloud/org-mode/emacs1.org" "/Users/fernando/Sync-pCloud/org-mode/email.org" "/Users/fernando/Sync-pCloud/org-mode/encryption.org" "/Users/fernando/Sync-pCloud/org-mode/english.org" "/Users/fernando/Sync-pCloud/org-mode/functional-programming.org" "/Users/fernando/Sync-pCloud/org-mode/gamedev.org" "/Users/fernando/Sync-pCloud/org-mode/gaming.org" "/Users/fernando/Sync-pCloud/org-mode/gdb.org" "/Users/fernando/Sync-pCloud/org-mode/gimp-inkscape.org" "/Users/fernando/Sync-pCloud/org-mode/git.org" "/Users/fernando/Sync-pCloud/org-mode/go.org" "/Users/fernando/Sync-pCloud/org-mode/goals.org" "/Users/fernando/Sync-pCloud/org-mode/hacking.org" "/Users/fernando/Sync-pCloud/org-mode/haskell-exercises.org" "/Users/fernando/Sync-pCloud/org-mode/haskell-programming-from-first-principles.org" "/Users/fernando/Sync-pCloud/org-mode/haskell.org" "/Users/fernando/Sync-pCloud/org-mode/how-to-learn.org" "/Users/fernando/Sync-pCloud/org-mode/html-css.org" "/Users/fernando/Sync-pCloud/org-mode/humans.org" "/Users/fernando/Sync-pCloud/org-mode/imagemagick.org" "/Users/fernando/Sync-pCloud/org-mode/images.org" "/Users/fernando/Sync-pCloud/org-mode/javascript.org" "/Users/fernando/Sync-pCloud/org-mode/jekyllrb.org" "/Users/fernando/Sync-pCloud/org-mode/learn-you-haskell.org" "/Users/fernando/Sync-pCloud/org-mode/learning.org" "/Users/fernando/Sync-pCloud/org-mode/life.org" "/Users/fernando/Sync-pCloud/org-mode/linux-desktop.org" "/Users/fernando/Sync-pCloud/org-mode/lisp-scheme.org" "/Users/fernando/Sync-pCloud/org-mode/mariadb.org" "/Users/fernando/Sync-pCloud/org-mode/math.org" "/Users/fernando/Sync-pCloud/org-mode/movies.org" "/Users/fernando/Sync-pCloud/org-mode/music.org" "/Users/fernando/Sync-pCloud/org-mode/networking.org" "/Users/fernando/Sync-pCloud/org-mode/newsletters.org" "/Users/fernando/Sync-pCloud/org-mode/numeric-systems.org" "/Users/fernando/Sync-pCloud/org-mode/orgmode.org" "/Users/fernando/Sync-pCloud/org-mode/people.org" "/Users/fernando/Sync-pCloud/org-mode/php.org" "/Users/fernando/Sync-pCloud/org-mode/ponteiros-lista-encadeada-didi.org" "/Users/fernando/Sync-pCloud/org-mode/programming.org" "/Users/fernando/Sync-pCloud/org-mode/racket.org" "/Users/fernando/Sync-pCloud/org-mode/regex.org" "/Users/fernando/Sync-pCloud/org-mode/religion.org" "/Users/fernando/Sync-pCloud/org-mode/rsync.org" "/Users/fernando/Sync-pCloud/org-mode/ruby.org" "/Users/fernando/Sync-pCloud/org-mode/shell.org" "/Users/fernando/Sync-pCloud/org-mode/temp.org" "/Users/fernando/Sync-pCloud/org-mode/tempnotes.org" "/Users/fernando/Sync-pCloud/org-mode/terminal.org" "/Users/fernando/Sync-pCloud/org-mode/time-management.org" "/Users/fernando/Sync-pCloud/org-mode/tmux.org" "/Users/fernando/Sync-pCloud/org-mode/tomb-raider-vocabulary.org" "/Users/fernando/Sync-pCloud/org-mode/tombraider.org" "/Users/fernando/Sync-pCloud/org-mode/tools.org" "/Users/fernando/Sync-pCloud/org-mode/unicode.org" "/Users/fernando/Sync-pCloud/org-mode/vbserp.org" "/Users/fernando/Sync-pCloud/org-mode/vim.org" "/Users/fernando/Sync-pCloud/org-mode/webpack.org" "/Users/fernando/Sync-pCloud/org-mode/wget.org" "/Users/fernando/Sync-pCloud/org-mode/wisdom.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/2021-Vocabulary.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/E-mail.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/IRC.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/README.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/anki-srs.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/backup.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/electron.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/emacs.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/fzf.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/git-config.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/git-general-concepts.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/git-useful-commands.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/grep.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/insights.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/jobs-companies.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/kitty.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/lisp-general.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/project-ideas.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/quotes-wisdom.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/ripgrep.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/scratchpad.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/ssh-useful.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/terminal-emulator.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/tmux.org" "/Users/fernando/Sync-pCloud/Org-Notes/Personal/tools.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/GAP-Norming.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Gap-Git.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/GapInc-Ideas.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/GapInc-On-Call.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/GapInc-Tasks.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/GapInc-cmdline.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/GapInc.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Growth.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Interviewing.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Leapfrog.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Norming Session.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Security Champions GapInc.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Splunk-Fundamentals-1.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/TW-2021-06.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/TW-2021-07.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/TW-2021-08.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/TW-ETG.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/TW-Transitional-Sponsor.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/TW-projects.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Thoughtworks.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/Timecard.org" "/Users/fernando/Sync-pCloud/Org-Notes/Thoughtworks/sitemap.org" "/Users/fernando/Sync-pCloud/Org-Notes/Goals.org" "/Users/fernando/Sync-pCloud/Org-Notes/org.org"))
+ '(package-selected-packages
+   '(ob-typescript emojify helm-org-ql org-ql imenu-list markdown-mode yasnippet helm-rg helm-projectile helm-ag helm heml sublimity-scroll sublimity-attractive sublimity diff-hl git-gutter solarized-theme typescript-mode editorconfig geiser gruvbox-theme projectile neotree paredit haskell-mode org-plus-contrib magit htmlize zenburn-theme color-theme which-key racket-mode use-package))
  '(safe-local-variable-values '((org-confirm-babel-evaluate))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -152,7 +154,23 @@
 (use-package org
   :mode (("\\.org$" . org-mode))
   :ensure org-plus-contrib
-  :config)
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (shell . t)
+     (scheme . t)
+     (ruby . t)
+     (haskell . t)
+     (typescript . t))))
+
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq-default typescript-indent-level 2))
+
+(use-package ob-typescript
+  :ensure t)
 
 (use-package editorconfig
   :ensure t
@@ -239,14 +257,6 @@
   (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
   (show-paren-mode 1))
 
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (shell . t)
-   (scheme . t)
-   (ruby . t)
-   (haskell . t)))
 
 ;; Ask (t) or don't ask (nil) for confirmation to evaluate?
 (setq org-confirm-babel-evaluate nil)

c77e375f5393142cdd7562fdadbddbcf26173b48
Fernando Basso <fernandobasso.br@gmail.com>
2022-01-08 08:05:42 -0300

    chore: Refactor sync.sh to make it more understandable

--- sync.sh
+++ sync.sh
@@ -1,36 +1,32 @@
 #!/usr/bin/env bash
 
-##
-# ENV VARS
-# ========
 #
-# • DOTFILES_FETCH_REPOS: If set, clone or update stuff that depend
-# on code repositories. Example:
+# Simply switch to <this repo> root directory and run:
 #
-#   $ DOTFILES_FETCH_REPOS=1 bash sync.sh
+#   $ bash sync.sh
 #
 
 dir_script="$(cd "$(dirname "$0")" && pwd -P)"
 
 ##
-# Files that can be symblinked in a simple and straightforward way.
+# This functio symblinks config files to their respective expected
+# locations under the user's $HOME directory.
 #
-deploy () {
-  while read -r file ; do
-    mkdir -pv "$(dirname "$HOME/$file")"
+symblink_files () {
+	while read -r file ; do
+		mkdir -pv "$(dirname "$HOME/$file")"
 
-    if [ ! -L "$file" ]; then
-      if [ -e "$HOME/$file" ]; then
-        printf '%s\n' "[SKIP]: ~/$file exists and is NOT a symblink..."
-      else
-        printf '%s\n' "[SYMB]: ~/$file is bing symblinked"
-        ln -sv "$dir_script/$file" "$HOME/$file"
-      fi
-    fi
-  done < ./MANIFEST-LINUX.txt
+		if [ ! -L "$file" ]; then
+			if [ -e "$HOME/$file" ]; then
+				printf '%s\n' "[SKIP]: ~/$file exists and is NOT a symblink..."
+			else
+				printf '%s\n' "[SYMB]: ~/$file is bing symblinked"
+				ln -sv "$dir_script/$file" "$HOME/$file"
+			fi
+		fi
+	done < ./MANIFEST-LINUX.txt
 }
 
-deploy
-
+#
 # vim: set tabstop=2 softtabstop=2 shiftwidth=2:
-
+#

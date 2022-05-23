8967a24afc8189b2fe55a72869b386d944c12ffa
Fernando Basso <fernandobasso.br@gmail.com>
2022-05-23 07:22:37 -0300

    script: Add shell script to open my math study session apps

---
+++ bin/math-apps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+dir="$HOME/Cloud/pCloud/Handwritten-Notes/Math/Algebra 1"
+
+gnome-calculator &
+
+thunar "$dir" &
+
+terminator \
+  --title 'MATH REPL' \
+  --profile GruvboxLight \
+	--working-directory ~/Public \
+  --geometry=400x500-0-0 \
+	-e 'stack ghci'
+

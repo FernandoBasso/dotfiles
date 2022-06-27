385f6a6985529309660c83be69c5348d83f9592b
Fernando Basso <fernandobasso.br@gmail.com>
2022-06-27 09:01:16 -0300

    nvim: Add some settings for working with plugin windows
    I sometimes have both Nvim Tree and Symbols Outline open (one on each
    side of the main editing window). If they are open, and I want to use
    vim fugitive window, I first close those. Added some functions and
    mappings to make the process easier.
    
    - `F3` to toggle nvim-tree.
    - `F4` to toggle symbols outline.
    - <Leader>gg to close the side windows and then open vim fugitive's git
      window so we end up with a nice, usable window layout.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -537,8 +537,33 @@ cmp.setup {
 ------------------------------------------------------------------------------
 -- nvim-tree
 --
-vim.keymap.set('n', '<F3>', ':NvimTreeToggle<CR>')
+-- vim.keymap.set('n', '<F3>', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<Leader>ff', ':NvimTreeFindFile')
+
+------------------------------------------------------------------------------
+-- Toggle Nvim Tree and Symbols outline
+--
+vim.keymap.set('n', '<F3>', function() vim.api.nvim_command('NvimTreeToggle') end)
+vim.keymap.set('n', '<F4>', function() vim.api.nvim_command('SymbolsOutline') end)
+
+------------------------------------------------------------------------------
+-- Vim Fugitive Goodies
+--
+-- When I have nvim-tree and symbols outline open on both sides of the
+-- main editing window, I want to first close them before opening vim
+-- fugitive Git window on a vertical split.
+--
+-- Vertical because I like having git info on the left, and I can then
+-- open buffers under inspections on the split on the right so I can see
+-- the changes in more context when needed.
+--
+local vert_git = function()
+  vim.api.nvim_command('NvimTreeClose')
+  vim.api.nvim_command('SymbolsOutlineClose')
+  vim.api.nvim_command('vertical Git')
+end
+
+vim.keymap.set('n', '<Leader>gg', vert_git)
 
 --
 -- vim: tw=72 ts=2 sts=2 sw=2 et

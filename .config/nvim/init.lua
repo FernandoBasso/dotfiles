bb562f63eda1d7b7fea1fe5b079c354bb04125b6
Fernando Basso <fernandobasso.br@gmail.com>
2022-06-28 08:38:57 -0300

    nvim: Make lualine config fancier
    - Enable icons.
    - Use default separators.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,7 +33,10 @@ require('packer').startup(function(use)
   use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }               -- Autocompletion
   use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } }           -- Snippet Engine and Snippet Expansion
   use 'sainnhe/gruvbox-material'
-  use 'nvim-lualine/lualine.nvim'                                                 -- Fancier statusline
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+  }
   use 'lukas-reineke/indent-blankline.nvim'                                       -- Add indentation guides even on blank lines
   use 'tpope/vim-sleuth'                                                          -- Detect tabstop and shiftwidth automatically
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Fuzzy Finder (files, lsp, etc)
@@ -176,10 +179,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help lualine.txt`
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'gruvbox-material',
-    component_separators = '|',
-    section_separators = '',
+    -- component_separators = '|',
+    -- section_separators = '',
   },
 }
 

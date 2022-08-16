1718a44bdd7c7de4bf9cffedc300ca381c0e0284
Fernando Basso <fernandobasso.br@gmail.com>
2022-08-16 07:32:21 -0300

    nvim: Add editorconfig plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,9 @@ local util = require('lspconfig.util')
 -- stylua: ignore start
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'                                                    -- Package manager
+
+  use 'gpanders/editorconfig.nvim'
+
   use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }       -- Add git related info in the signs columns and popups
   use 'numToStr/Comment.nvim'                                                     -- "gc" to comment visual regions/lines

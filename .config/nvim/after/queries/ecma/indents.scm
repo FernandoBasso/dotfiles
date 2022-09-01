12eab05b789983fd4df849f3cb3096f2f41068d7
Fernando Basso <fernandobasso.br@gmail.com>
2022-09-01 06:45:49 -0300

    nvim: Add config to fix JS indentation for JSDoc

---
+++ .config/nvim/after/queries/ecma/indents.scm
@@ -0,0 +1,20 @@
+;;
+;; Check:
+;;
+;; • https://github.com/nvim-treesitter/nvim-treesitter/issues/1167
+;; • https://github.com/nvim-treesitter/nvim-treesitter/pull/2545
+;;
+;; Treesitter has to have `indent` enabled and JSDoc comments have to
+;; be in the form:
+;;
+;;   /**
+;;    *
+;;    */
+;;
+;; An unfinished (unclosed) JSDoc will cause the indentantion to
+;; behave incorrectly.
+;;
+[
+  (comment)
+  (template_string)
+] @auto

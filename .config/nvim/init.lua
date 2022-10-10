91bf87e3916e2233c09182573a88cf70d46f36a2
Fernando Basso <fernandobasso.br@gmail.com>
2022-10-10 14:58:45 -0300

    nvim: Add lspconfig in place of coc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -353,58 +353,116 @@ require('nvim-treesitter.configs').setup {
 ------------------------------------------------------------------------
 -- CoC — Conquer of Completion
 -- ---------------------------
-vim.cmd [[
-let g:coc_global_extensions = [
-      \ 'coc-diagnostic',
-      \ 'coc-json',
-      \ 'coc-yaml',
-      \ 'coc-tsserver',
-      \ 'coc-eslint',
-      \ 'coc-solargraph',
-      \ 'coc-css',
-      \ ]
+-- vim.cmd [[
+-- let g:coc_global_extensions = [
+--       \ 'coc-diagnostic',
+--       \ 'coc-json',
+--       \ 'coc-yaml',
+--       \ 'coc-tsserver',
+--       \ 'coc-eslint',
+--       \ 'coc-solargraph',
+--       \ 'coc-css',
+--       \ ]
+--
+-- "
+-- " “Go To” code navigation.
+-- "
+-- nmap <silent> gd <Plug>(coc-definition)
+-- nmap <silent> gy <Plug>(coc-type-definition)
+-- nmap <silent> gi <Plug>(coc-implementation)
+-- nmap <silent> gr <Plug>(coc-references)
+--
+-- "
+-- " Suggest semantic completions with Ctrl+Tab. Works really well,
+-- " and handles suggestions of object properties. Example:
+-- "
+-- " type TParams {
+-- "   url: string,
+-- "   method: string,
+-- " }
+-- "
+-- " Now, typing “const params: TParams { <C-Space>” displays
+-- " url, method, and their types and even documentation,
+-- " if available.
+-- "
+-- inoremap <silent><expr> <C-Space> coc#refresh()
+--
+-- "
+-- " Display diagnostics (errors and warnings) and documentation.
+-- " Type uppercase ‘K’ to activate, and any motion (like ‘j’, ‘k’,
+-- " ‘h’, ‘b’, ‘<C-o>’, ‘<C-i>’, etc.) to deactivate.
+-- "
+-- nnoremap <silent> K :call CocAction('doHover')<CR>
+--
+-- "
+-- " Navigate to next/prev errors.
+-- "
+-- nmap <silent> [g <Plug>(coc-diagnostic-prev)
+-- nmap <silent> ]g <Plug>(coc-diagnostic-next)
+--
+-- "
+-- " Rename symbol
+-- "
+-- nmap <leader>rn <Plug>(coc-rename)
+-- ]]
 
-"
-" “Go To” code navigation.
-"
-nmap <silent> gd <Plug>(coc-definition)
-nmap <silent> gy <Plug>(coc-type-definition)
-nmap <silent> gi <Plug>(coc-implementation)
-nmap <silent> gr <Plug>(coc-references)
+------------------------------------------------------------------------
+-- lsp-config
+--
+--
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-"
-" Suggest semantic completions with Ctrl+Tab. Works really well,
-" and handles suggestions of object properties. Example:
-"
-" type TParams {
-"   url: string,
-"   method: string,
-" }
-"
-" Now, typing “const params: TParams { <C-Space>” displays
-" url, method, and their types and even documentation,
-" if available.
-"
-inoremap <silent><expr> <C-Space> coc#refresh()
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-"
-" Display diagnostics (errors and warnings) and documentation.
-" Type uppercase ‘K’ to activate, and any motion (like ‘j’, ‘k’,
-" ‘h’, ‘b’, ‘<C-o>’, ‘<C-i>’, etc.) to deactivate.
-"
-nnoremap <silent> K :call CocAction('doHover')<CR>
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
 
-"
-" Navigate to next/prev errors.
-"
-nmap <silent> [g <Plug>(coc-diagnostic-prev)
-nmap <silent> ]g <Plug>(coc-diagnostic-next)
-
-"
-" Rename symbol
-"
-nmap <leader>rn <Plug>(coc-rename)
-]]
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+require('lspconfig')['tsserver'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require'lspconfig'.purescriptls.setup {
+  on_attach = on_attach,
+  settings = {
+    purescript = {
+      addSpagoSources = true
+    }
+  },
+  flags = {
+    debounce_text_changes = 150,
+  }
+}
 
 ------------------------------------------------------------------------------
 -- nvim-tree

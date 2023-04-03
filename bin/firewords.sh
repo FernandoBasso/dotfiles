de6832e0f9cf9f12a685090b86a695b65c6c263c
Fernando Basso <fernandobasso.br@gmail.com>
2023-04-03 09:13:40 -0300

    script: Improve firewords.sh
    Commit persdict.txt generated file.

--- bin/firewords.sh
+++ bin/firewords.sh
@@ -8,13 +8,13 @@
 # effect on a given page.
 #
 
-files=($(find ~/.mozilla -iname persdict.dat))
+persdict_files=($(find ~/.mozilla -iname persdict.dat))
 date="$(date +'%Y-%m-%d-%M-%S')"
 
 ##
 # Add all words from all persdict.dat into a single file.
 #
-for file in "${files[@]}"
+for file in "${persdict_files[@]}"
 do
   cp "$file" "${file}-${date}.bkp"
 
@@ -24,15 +24,15 @@ done
 ##
 # Sort and dedup the entries.
 #
-cat ./words.txt | sort | uniq > ./deduped-words.txt
+cat ./words.txt | sort | uniq > ./persdict.txt
 
 ##
 # Copy the sorted and deduped resulting file as persdict.dat
 # in each of the profile directries where the original dict
 # files were found in the first place.
 #
-for file in "${files[@]}"
+for file in "${persdict_files[@]}"
 do
-  cp -v ./deduped-words.txt "$file"
+  cp -v ./persdict.txt "$file"
   wc -l "$file"
 done

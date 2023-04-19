17388cceb03cd7f6b4361decf04a188a1da2e55b
Fernando Basso <fernandobasso.br@gmail.com>
2023-04-19 09:11:55 -0300

    emacs: Install and config deft

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -229,6 +229,17 @@
   (setq-default )
   (org-roam-setup))
 
+(use-package deft
+  :ensure t
+  :after org
+  :bind
+  ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory org-roam-directory))
+
 (use-package org-download
   :ensure t
   :after org

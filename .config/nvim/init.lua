577086ddcbd7ad84b8d7ed83812e91e6dbb5399b
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-14 08:56:08 -0300

    nvim: Add haskell-tools deps and confs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,8 @@ end
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'                                                    -- Package manager
 
+  use 'neovim/nvim-lspconfig'
+
   use 'gpanders/editorconfig.nvim'
 
   use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
@@ -35,6 +37,15 @@ require('packer').startup(function(use)
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+  }
+
+  use {
+    'mrcjkb/haskell-tools.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      -- 'nvim-telescope/telescope.nvim', -- optional
+    },
+    branch = '1.x.x', -- recommended
   }
 
   --
@@ -210,7 +221,7 @@ require("gruvbox").setup({
   },
 })
 
-vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme gruvbox")
 
 -- require("gruvbox").setup({
 --   undercurl = true,
@@ -292,6 +303,57 @@ require('Comment').setup()
 --   char = '┊',
 --   show_trailing_blankline_indent = false,
 -- }
+
+
+local lspconfig = require('lspconfig')
+-- lspconfig.pyright.setup {}
+-- lspconfig.tsserver.setup {}
+-- lspconfig.rust_analyzer.setup {
+--   -- Server-specific settings. See `:help lspconfig-setup`
+--   settings = {
+--     ['rust-analyzer'] = {},
+--   },
+-- }
+
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
 
 ------------------------------------------------------------------------------
 -- ## Gitsigns
@@ -525,6 +587,12 @@ local grep_ts = function()
 end
 
 vim.keymap.set('n', '<Leader>\\gt', grep_ts)
+
+------------------------------------------------------------------------
+-- Haskell Tools
+--
+-- See ~/.config/nvim/ftplugin/haskell.lua
+--
 
 ------------------------------------------------------------------------
 -- FZF

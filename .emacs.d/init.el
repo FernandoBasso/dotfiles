5e3fcdf254711b388a43a85831a8d584c3aa5e69
Fernando Basso <fernandobasso.br@gmail.com>
2023-08-01 18:18:53 -0300

    emacs: Add some tree-sitter grammars for emacs 29

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -712,3 +712,24 @@
  'org-block nil
  :background "#fbf2d3")
 
+(setq treesit-language-source-alist
+   '((c "https://github.com/tree-sitter/tree-sitter-c")
+     (haskell "https://github.com/tree-sitter/tree-sitter-haskell")
+     (bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")))
+
+; (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+

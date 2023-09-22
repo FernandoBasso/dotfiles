667cad8f7a3ae8007958fea4d7a333b0f8b4cf5a
Fernando Basso <fernandobasso.br@gmail.com>
2023-09-22 09:02:36 -0300

    nvim: Add lsp providers and change some keybindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -335,15 +335,30 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
   end,
 })
+
+------------------------------------------------------------------------
+-- lsp-config typescript-language-server
+--
+require'lspconfig'.tsserver.setup{}
+
+------------------------------------------------------------------------
+-- lsp-config clangd
+--
+require'lspconfig'.clangd.setup{}
+
+------------------------------------------------------------------------
+-- lsp-config eslint_lsp
+--
+require'lspconfig'.eslint.setup{}
 
 ------------------------------------------------------------------------
 -- nvim-treesitter/nvim-treesitter-context

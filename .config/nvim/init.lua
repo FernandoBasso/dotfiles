efda7f91887d06e7e7ddd7bc62c9d1785873ed59
Fernando Basso <fernandobasso.br@gmail.com>
2023-09-28 12:30:18 -0300

    nvim: Remove buffer from cmdline cmp completions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -440,7 +440,6 @@ cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' },
-    { name = 'buffer' },
   }, {
     { name = 'cmdline' }
   })

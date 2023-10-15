96d03110dd020d27d8a858ad4f5f2b1428d1c8b4
Fernando Basso <fernandobasso.br@gmail.com>
2023-10-15 08:37:37 -0300

    nvim: Disable some plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,7 +28,7 @@ require('packer').startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'rasulomaroff/cmp-bufname'
   use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
+  -- use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
   use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/cmp-nvim-lsp-signature-help'

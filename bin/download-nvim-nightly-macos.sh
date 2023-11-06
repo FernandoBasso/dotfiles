a9409a281fc72613415d213805a6adce6df12673
Fernando Basso <fernandobasso.br@gmail.com>
2023-11-06 09:28:38 -0300

    bash: Script to download nvim lightly for macOS

---
+++ bin/download-nvim-nightly-macos.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+{
+  cd ~/bin
+
+  wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz -O ./nvim-macos.tar.gz
+
+  xattr -c ./nvim-macos.tar.gz
+
+  tar zxvf ./nvim-macos.tar.gz
+
+  rm -rv ./nvim-macos.tar.gz
+
+  ln -sv ~/bin/nvim-macos/bin/nvim ~/bin/nv
+}

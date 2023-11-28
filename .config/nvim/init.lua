c5cac9c81e7864fc9c7abebbb35a2e0698193873
Fernando Basso <fernandobasso.br@gmail.com>
2023-11-28 17:48:00 -0300

    nvim: Display relative file path in lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -300,6 +300,11 @@ require('lualine').setup {
     -- component_separators = '|',
     -- section_separators = '',
   },
+  sections = {
+    lualine_c = {
+      { 'filename', path = 1 }
+    }
+  }
 }
 
 -- Enable Comment.nvim

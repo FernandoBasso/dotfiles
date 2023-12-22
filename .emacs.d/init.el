2a53223c4148202986f7b942f0c889b2decda59c
Fernando Basso <fernandobasso.br@gmail.com>
2023-12-22 08:02:17 -0300

    emacs: Add tree-sitter stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -715,24 +715,15 @@
  'org-block nil
  :background "#fbf2d3")
 
-(setq treesit-language-source-alist
-   '((c "https://github.com/tree-sitter/tree-sitter-c")
-     (haskell "https://github.com/tree-sitter/tree-sitter-haskell")
-     (bash "https://github.com/tree-sitter/tree-sitter-bash")
-     (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (css "https://github.com/tree-sitter/tree-sitter-css")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     (go "https://github.com/tree-sitter/tree-sitter-go")
-     (html "https://github.com/tree-sitter/tree-sitter-html")
-     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-     (json "https://github.com/tree-sitter/tree-sitter-json")
-     (make "https://github.com/alemuller/tree-sitter-make")
-     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml")
-     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; tree-sitter
+;;
 
-; (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+(use-package tree-sitter
+  :ensure t)
 
+(use-package tree-sitter-langs
+  :ensure t)
+
+(global-tree-sitter-mode)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)

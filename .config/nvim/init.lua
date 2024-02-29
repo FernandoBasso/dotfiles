c312430a102b2189938eff596722332c22217ba5
Fernando Basso <fernandobasso.br@gmail.com>
2024-02-29 09:56:06 -0300

    nvim: Update lsp diagnostic icons

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -453,8 +453,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
 -- LSP diagnostics
 --
 vim.diagnostic.config({
-  virtual_text = false
-});
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 ------------------------------------------------------------------------
 -- lsp-config typescript-language-server

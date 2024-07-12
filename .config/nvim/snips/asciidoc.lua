5e77ae190883ebbd1514e04257ca62347843e5e7
Fernando Basso <fernandobasso.br@gmail.com>
2024-07-12 08:01:09 -0300

    nvim: Add asciidoc ini with gh icons snippet

--- .config/nvim/snips/asciidoc.lua
+++ .config/nvim/snips/asciidoc.lua
@@ -54,6 +54,33 @@ local adoc_snips = {
     'exercismle',
     fmt(exercismle, { i(1), i(2), i(3), i(4), rep(1), rep(2) })
   ),
+
+  s(
+    'adocini',
+    fmt(
+      [[
+        = {}
+        :page-subtitle: {}
+        :page-tags: {}
+        :favicon: https://fernandobasso.dev/cmdline.png
+        :icons: font
+        :sectlinks:
+        :sectnums!:
+        :toclevels: 6
+        :toc: left
+        :source-highlighter: highlight.js
+        :imagesdir: __assets
+        :stem: latexmath
+        ifdef::env-github[]
+        :tip-caption: :bulb:
+        :note-caption: :information_source:
+        :important-caption: :heavy_exclamation_mark:
+        :caution-caption: :fire:
+        :warning-caption: :warning:
+        endif::[]
+      ]], { i(1), i(2), i(3) }
+    )
+  )
 }
 
 ls.add_snippets('asciidoctor', adoc_snips, { key = 'adoc' })

261c011225a9b4ae8e82df99e61b14586bcc810c
Fernando Basso <fernandobasso.br@gmail.com>
2024-07-25 08:37:01 -0300

    nvim: Truncate telescope file results

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -63,11 +63,9 @@ return {
       -- You can put your default mappings / updates / etc. in here All
       -- the info you're looking for is in :help telescope.setup().
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        path_display = { 'truncate' }
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {

0eedf69e02175c45cfec881f9c242a70ed67b2bd
Fernando Basso <fernandobasso.br@gmail.com>
2024-08-24 07:51:08 -0300

    nvim: Add Go format on save config

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -8,6 +8,18 @@ return {
   },
 
   config = function()
+    ----
+    -- Format on save.
+    --
+    local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*.go',
+      callback = function()
+       require('go.format').gofmt()
+      end,
+      group = format_sync_grp,
+    })
+
     vim.keymap.set(
       'n',
       '<Leader>;',

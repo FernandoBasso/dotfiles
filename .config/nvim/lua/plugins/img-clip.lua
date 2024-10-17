26683f96537762c569bf51f860d22b794db34658
Fernando Basso <fernandobasso.br@gmail.com>
2024-10-17 09:25:32 -0300

    nvim: Add img-clip plugin with config for AsciiDoc[tor]

---
+++ .config/nvim/lua/plugins/img-clip.lua
@@ -0,0 +1,43 @@
+return {
+  'HakonHarnes/img-clip.nvim',
+  event = 'VeryLazy',
+  opts = {
+    relative_to_current_file = true,
+    filetypes = {
+      asciidoctor = {
+        ----
+        -- Make the image directory location relative to the location of
+        -- the current buffer. For example, if we have:
+        --
+        --   ~/source/myproj/docs/cmdline/sed-intro.adoc
+        --
+        -- The, the image is place under:
+        --
+        --   ~/source/myproj/docs/cmdline/__assets/<image-name>.png
+        --
+        -- For AsciiDoc[tor], I like to always keep the images under an
+        -- `__assets/` directory relative to the current file.
+        --
+        dir_path = function()
+          return vim.fn.expand('%:.:h') .. '/__assets'
+        end,
+
+        url_encode_path = true, ---@type boolean | fun(): boolean
+
+        ----
+        -- • https://docs.asciidoctor.org/asciidoc/latest/macros/images/
+        --
+        template = "image::$FILE_PATH[$CURSOR]", ---@type string | fun(context: table): string
+
+        download_images = false, ---@type boolean | fun(): boolean
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader><leader>pi',
+      '<cmd>PasteImage<cr>',
+      desc = 'Paste image from system clipboard',
+    },
+  },
+}

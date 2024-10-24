be0b48993bf3a7afe03901f32ea2621c57700021
Fernando Basso <fernandobasso.br@gmail.com>
2024-10-23 21:02:07 -0300

    kitty(nvim): Use gruvbox hard light theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -337,6 +337,7 @@ require('lazy').setup({
       config = function()
         -- vim.g.gruvbox_material_enable_italic = false
         vim.g.gruvbox_material_foreground = 'material'
+        vim.g.gruvbox_material_background = 'hard'
         vim.g.gruvbox_material_enable_italic = false
         vim.g.gruvbox_material_enable_italic = 0
         vim.g.gruvbox_material_disable_italic_comment = 1

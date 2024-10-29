ab1788fa1a1391a59cc37a7dd3c1c634e59a0869
Fernando Basso <fernandobasso.br@gmail.com>
2024-10-29 13:17:01 -0300

    nvim: Add restore-cursor autocmd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -145,6 +145,21 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 6
 
 ------------------------------------------------------------------------
+-- Restore cursor position.
+--
+vim.cmd [[
+  augroup RestoreCursor
+    autocmd!
+    autocmd BufRead * autocmd FileType <buffer> ++once
+      \ let s:line = line("'\"")
+      \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
+      \      && index(['xxd', 'gitrebase'], &filetype) == -1
+      \ |   execute "normal! g`\""
+      \ | endif
+  augroup END
+]]
+
+------------------------------------------------------------------------
 -- Basic Keymaps
 --
 -- • :help vim.keymap.set()

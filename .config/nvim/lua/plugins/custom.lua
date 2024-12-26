267fecf89967a37e36cc876c58e54fdbb91af488
Fernando Basso <fernandobasso.br@gmail.com>
2024-12-26 20:36:40 -0300

    nvim: Make fzf-lua window larger

---
+++ .config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      winopts = {
+        width = 0.95,
+        height = 0.9,
+        row = 0.5,
+        col = 0.5,
+        preview = {
+          scrollchars = { "·", "" },
+        },
+      },
+    },
+  },
+}

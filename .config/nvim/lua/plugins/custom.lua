335bd93bf1ee5b529b4f29a484beacb784202949
Fernando Basso <fernandobasso.br@gmail.com>
2024-12-27 11:05:35 -0300

    nvim: Point LuaSnip to my custom snippets directory

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -13,4 +13,19 @@ return {
       },
     },
   },
+
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = false,
+    config = function()
+      require("luasnip.loaders.from_lua").load({
+        paths = { "~/.config/nvim/snips/" },
+      })
+
+      require("luasnip").config.setup({
+        update_events = "TextChanged,TextChangedI",
+        enable_autosnippets = true,
+      })
+    end,
+  },
 }

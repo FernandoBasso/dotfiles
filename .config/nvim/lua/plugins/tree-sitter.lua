8de5509464b2f338fcccfa7ec6ada3a0759b9f10
Fernando Basso <fernandobasso.br@gmail.com>
2025-02-19 10:11:23 -0300

    nvim: Add tree-sitter-text-objects

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,5 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   build = ':TSUpdate',
   opts = {
     ensure_installed = {
@@ -56,27 +59,78 @@ return {
         'go'
       },
     },
+    textobjects = {
+      move = {
+        enable = true,
+
+        ----
+        -- whether to set jumps in the jumplist
+        --
+        set_jumps = true,
+
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+
+          ["]]"] = { query = "@class.outer", desc = "Next class start" },
+
+          ----
+          -- You can use regex matching (i.e. lua pattern) and/or pass a
+          -- list in a "query" key to group multiple queries.
+          --
+          ["]o"] = "@loop.*",
+          -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+          ----
+
+          ----
+          -- You can pass a query group to use query from
+          -- `queries/<lang>/<query_group>.scm file in your runtime path.
+          --
+          -- Below example nvim-treesitter's `locals.scm` and
+          -- `folds.scm`. They also provide highlights.scm and
+          -- indent.scm.
+          --
+          ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+
+          ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+        },
+
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+
+        ----
+        -- Below will go to either the start or the end, whichever is closer.
+        -- Use if you want more granular movements
+        -- Make it even more gradual by adding multiple queries and regex.
+        goto_next = {
+          ["]d"] = "@conditional.outer",
+        },
+        goto_previous = {
+          ["[d"] = "@conditional.outer",
+        }
+      },
+    },
   },
+
   config = function(_, opts)
     ----
     -- See `:help nvim-treesitter`
     -- Prefer git instead of curl in order to improve connectivity in
     -- some environments.
+    --
     require('nvim-treesitter.install').prefer_git = true
+
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup(opts)
-
-    ----
-    -- There are additional nvim-treesitter modules that you can use to
-    -- interact with nvim-treesitter. You should go explore a few and
-    -- see what interests you:
-    --
-    -- • Incremental selection: Included, see
-    --   `:help nvim-treesitter-incremental-selection-mod`
-    -- • Show your current context:
-    --   https://github.com/nvim-treesitter/nvim-treesitter-context
-    -- • Treesitter + textobjects:
-    --   https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    --
   end
 }

7a8c498727a9c521db2395ccdbf5913e0cdf40f7
Fernando Basso <fernandobasso.br@gmail.com>
2025-03-18 19:36:34 -0300

    nvim: Change some tree-sitter keymaps to not conflict with lsp diagnostics
    I use `]d` and `[d` to go to next and prev diagnostics, and use it much
    more than the tree-sitter related textobjects motions, so I'll keep
    these keymaps for the diagnostics.

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -110,13 +110,12 @@ return {
 
         ----
         -- Below will go to either the start or the end, whichever is closer.
-        -- Use if you want more granular movements
-        -- Make it even more gradual by adding multiple queries and regex.
+        --
         goto_next = {
-          ["]d"] = "@conditional.outer",
+          ["]D"] = "@conditional.outer",
         },
         goto_previous = {
-          ["[d"] = "@conditional.outer",
+          ["[D"] = "@conditional.outer",
         }
       },
     },

5a9ef945c7d2daed6b67227d3cd9f88d8400184b
Fernando Basso <fernandobasso.br@gmail.com>
2025-04-22 09:04:00 -0300

    git: Document fzf-related key bindings

--- bash_incl/git-helpers.sh
+++ bash_incl/git-helpers.sh
@@ -5,10 +5,24 @@
 # https://coderwall.com/p/ba8afa/git-branch-fuzzy-search-checkout
 #
 
-fbr() {
+_b() {
   local branches branch
   branches=$(git branch -a) &&
   branch=$(echo "$branches" | fzf +s +m) &&
   git checkout $(echo "$branch" | sed "s:.* remotes/origin/::" | sed "s:.* ::")
 }
 
+
+#
+# • https://stackoverflow.com/questions/36513310/how-to-get-a-gits-branch-with-fuzzy-finder
+#	• https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236
+#
+# But these are already working for me without anything else besides
+# having fzf and bash available.
+#
+# • CTRL-GCTRL-F: Files listed in git status
+# • CTRL-GCTRL-B: Branches
+# • CTRL-GCTRL-T: Tags
+# • CTRL-GCTRL-H: Commit hashes
+# • CTRL-GCTRL-R: Remotes
+#

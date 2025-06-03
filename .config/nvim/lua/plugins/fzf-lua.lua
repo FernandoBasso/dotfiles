837b3a558d56710ab7d45b05c7f39e3a661f30fe
Fernando Basso <fernandobasso.br@gmail.com>
2025-06-03 10:53:10 -0300

    nvim: Disable fzf-lua

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,71 +1,73 @@
-return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  config = function()
-    local fzflua = require('fzf-lua')
+return {}
 
-    fzflua.setup({
-      winopts = {
-        height = 0.96,
-        width = 0.94,
-        backdrop = 95,
-      },
-    })
-
-    ----
-    -- Trigger :Telescope.
-    --
-    vim.cmd [[nnoremap <Leader>F :FzfLua <C-d>]]
-    vim.cmd [[nnoremap <Leader>fl :FzfLua lsp_<C-d>]]
-
-    vim.keymap.set(
-      'n',
-      '<leader>sr',
-      fzflua.resume,
-      { desc = '[S]earch [R]esume' }
-    )
-
-    vim.keymap.set(
-      'n',
-      '<leader>sf',
-      fzflua.files,
-      { desc = '[S]earch [F]iles' }
-    )
-
-    vim.keymap.set(
-      'n',
-      '<leader>gr',
-      fzflua.lsp_references,
-      { desc = '[G]o to [R]eferences' }
-    )
-
-    vim.keymap.set(
-      'n',
-      '<leader>gi',
-      fzflua.lsp_implementations,
-      { desc = '[G]o to [I]mplementations' }
-    )
-
-    vim.keymap.set(
-      'n',
-      '<leader>gT',
-      fzflua.lsp_typedefs,
-      { desc = '[G]o to [T]ype defs' }
-    )
-
-    vim.keymap.set(
-      'n',
-      '<leader>ds',
-      fzflua.lsp_document_symbols,
-      { desc = '[D]ocument [S]ymbols' }
-    )
-
-    vim.keymap.set(
-      'n',
-      '<leader>ws',
-      fzflua.lsp_live_workspace_symbols,
-      { desc = 'Live [W]orkspace [S]ymbols' }
-    )
-  end
-}
+-- return {
+--   "ibhagwan/fzf-lua",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   opts = {},
+--   config = function()
+--     local fzflua = require('fzf-lua')
+--
+--     fzflua.setup({
+--       winopts = {
+--         height = 0.96,
+--         width = 0.94,
+--         backdrop = 95,
+--       },
+--     })
+--
+--     ----
+--     -- Trigger :Telescope.
+--     --
+--     vim.cmd [[nnoremap <Leader>F :FzfLua <C-d>]]
+--     vim.cmd [[nnoremap <Leader>fl :FzfLua lsp_<C-d>]]
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>sr',
+--       fzflua.resume,
+--       { desc = '[S]earch [R]esume' }
+--     )
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>sf',
+--       fzflua.files,
+--       { desc = '[S]earch [F]iles' }
+--     )
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>gr',
+--       fzflua.lsp_references,
+--       { desc = '[G]o to [R]eferences' }
+--     )
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>gi',
+--       fzflua.lsp_implementations,
+--       { desc = '[G]o to [I]mplementations' }
+--     )
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>gT',
+--       fzflua.lsp_typedefs,
+--       { desc = '[G]o to [T]ype defs' }
+--     )
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>ds',
+--       fzflua.lsp_document_symbols,
+--       { desc = '[D]ocument [S]ymbols' }
+--     )
+--
+--     vim.keymap.set(
+--       'n',
+--       '<leader>ws',
+--       fzflua.lsp_live_workspace_symbols,
+--       { desc = 'Live [W]orkspace [S]ymbols' }
+--     )
+--   end
+-- }

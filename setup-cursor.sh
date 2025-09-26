111e28c33d30dcac0c1d5339b1551e650ce53b00
Fernando Basso <fernandobasso.br@gmail.com>
2025-09-26 16:17:39 -0300

    cursor: Fix script to use Cursor insteadof Code

--- setup-cursor.sh
+++ setup-cursor.sh
@@ -4,17 +4,17 @@ echo '=============================='
 echo '===== Setting up Cursor ======'
 echo '=============================='
 echo "bash --------: $BASH_VERSION"
-echo "bash path ---: "$(which bash)"
+echo "bash path ---: $(which bash)"
 
 if [[ "$(uname)" == "Darwin" ]]
 then
 	ln -svf \
 		~/source/dotfiles/Cursor/User/keybindings.json \
-	  ~/Library/Application\ Support/Code/User/keybindings.json
+	  ~/Library/Application\ Support/Cursor/User/keybindings.json
 
 	ln -svf \
 		~/source/dotfiles/Cursor/User/settings.json \
-	  ~/Library/Application\ Support/Code/User/settings.json
+	  ~/Library/Application\ Support/Cursor/User/settings.json
 else
 	echo Linux
 	echo TODO Cursor setup

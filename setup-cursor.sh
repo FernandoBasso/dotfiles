02f618fa2f41268efd6409bc60feb8d4fc84a4e3
Fernando Basso <fernandobasso.br@gmail.com>
2025-09-26 16:12:34 -0300

    cursor: Add setup script

---
+++ setup-cursor.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo '=============================='
+echo '===== Setting up Cursor ======'
+echo '=============================='
+echo "bash --------: $BASH_VERSION"
+echo "bash path ---: "$(which bash)"
+
+if [[ "$(uname)" == "Darwin" ]]
+then
+	ln -svf \
+		~/source/dotfiles/Cursor/User/keybindings.json \
+	  ~/Library/Application\ Support/Code/User/keybindings.json
+
+	ln -svf \
+		~/source/dotfiles/Cursor/User/settings.json \
+	  ~/Library/Application\ Support/Code/User/settings.json
+else
+	echo Linux
+	echo TODO Cursor setup
+fi

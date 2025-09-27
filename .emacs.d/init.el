d46f3e816197c1660c642658bdfc589c1dcc551c
Fernando Basso <fernandobasso.br@gmail.com>
2025-09-27 07:04:47 -0300

    emacs: Refresh diff-hl fringes on magit commit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -686,7 +686,8 @@
 (use-package magit
   :ensure t
   :config
-  (global-set-key (kbd "C-x g") 'magit-status))
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 (use-package projectile
   :ensure t

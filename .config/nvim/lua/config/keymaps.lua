4e4b0887d3c83976febfdd268937b8f2c279ad3c
Fernando Basso <fernandobasso.br@gmail.com>
2025-10-14 17:31:23 -0300

    nvim: Add quickfix keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,54 @@
 vim.keymap.set('n', '<C-ç>', ':update<CR>')
 vim.keymap.set('i', '<C-ç>', '<Esc>:update<CR>')
 
+------------------------------------------------------------------------
+-- Quickfix ------------------------------------------------------------
+------------------------------------------------------------------------
+
+----
+-- Close the quickfix window.
+--
+vim.keymap.set(
+  'n',
+  '<Leader>qo',
+  function()
+    vim.cmd('copen')
+  end,
+  { desc = '[Q]uickfix [O]pen' }
+)
+
+----
+-- Close the quickfix window.
+--
+vim.keymap.set(
+  'n',
+  '<Leader>qc',
+  function()
+    vim.cmd('cclose')
+  end,
+  { desc = '[Q]uickfix [C]lose' }
+)
+
+----
+-- Next quickfix item
+--
+vim.keymap.set(
+  'n',
+  '<Leader>qn',
+  function()
+    vim.cmd('cnext')
+  end,
+  { desc = '[Q]uickfix [N]ext item' }
+)
+
+----
+-- Previous quickfix item
+--
+vim.keymap.set(
+  'n',
+  '<Leader>qp',
+  function()
+    vim.cmd('cprevious')
+  end,
+  { desc = '[Q]uickfix [P]revious item' }
+)

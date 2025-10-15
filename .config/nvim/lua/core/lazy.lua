e9adcb72eb7e8fe2c3800d59ef8b503f81497438
Fernando Basso <fernandobasso.br@gmail.com>
2025-10-15 09:53:45 -0300

    nvim: Enable zip/tar-related plugins
    Disabling those plugins would cuse go to definition error out and not be
    able to show files in Clojure projects that were installed as external,
    .jar dependencies.

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -1,9 +1,9 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 
-	local out = vim.fn.system({
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
@@ -12,38 +12,38 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath
   })
 
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ import = "plugins" }, {
-	install = {
-		missing = true,
-		colorscheme = { "habamax" },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
-	ui = {
-		border = "rounded",
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
+  install = {
+    missing = true,
+    colorscheme = { "habamax" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        -- "gzip",
+        -- "tarPlugin",
+        -- "zipPlugin",
+        "tohtml",
+        "tutor",
+      },
+    },
+  },
 })

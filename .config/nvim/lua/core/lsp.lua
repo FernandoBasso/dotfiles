c4f656b0142b6a88ffe48f9a02c0f1984387f2fb
Fernando Basso <fernandobasso.br@gmail.com>
2025-10-21 05:54:08 -0300

    nvmi: Add LSP signs (icons)

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -63,4 +63,13 @@ vim.diagnostic.config({
   virtual_lines = {
     current_line = true,
   },
+
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  },
 })

0d4f4c778a6583f76740a51975ea56ac458d3163
Fernando Basso <fernandobasso.br@gmail.com>
2025-10-22 16:38:35 -0300

    nvim: Fix conjure log in vsplit

--- .config/nvim/lua/config/clojure.lua
+++ .config/nvim/lua/config/clojure.lua
@@ -7,7 +7,7 @@ vim.g["conjure#completion#enabled"] = true
 vim.g["conjure#completion#omnifunc"] = "v:lua.require'blink.cmp'.omnifunc"
 vim.g["conjure#mapping#doc_word"] = false -- Let LSP handle K
 vim.g["conjure#log#hud#enabled"] = true -- Use buffer, not popup
-vim.g["conjure#mapping#log_vsplit"] = false
+vim.g["conjure#mapping#log_vsplit"] = "lv"
 vim.g["conjure#mapping#log_tab"] = false
 
 vim.g["conjure#mapping#session_select"] = false
@@ -22,10 +22,10 @@ vim.g["conjure#client#clojure#nrepl#test#current_form_names"] =
 	{ "deftest", "defflow", "defflow-i18n", "defspec", "facts" }
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = { "conjure-log-*" }, -- Conjure REPL buffer pattern
+	pattern = { "conjure-log-*" },
 	callback = function()
 		local total_cols = vim.o.columns
-		local target_width = math.floor(total_cols * 0.3)
+		local target_width = math.floor(total_cols * 0.4)
 		vim.cmd("vertical resize " .. target_width)
 	end,
 })

236c0af461c6e7f713f550fd644cedf96859be7f
Fernando Basso <fernandobasso.br@gmail.com>
2025-12-09 17:52:54 -0300

    nvim: Add snacks image

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,10 +3,27 @@ return {
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
+  keys = {
+    {
+      "<leader>Ii",
+      function()
+        print("HOVERING")
+        Snacks.image.hover()
+      end,
+      desc = "Show image in float window",
+    },
+  },
   opts = {
     bigfile = { enabled = false },
     dashboard = { enabled = false },
     explorer = { enabled = false },
+    image = {
+      enabled = true,
+      inline = false,
+      float = true,
+      max_width = 100,
+      max_height = 100,
+    },
     indent = { enabled = true },
     input = {
       enabled = true,
@@ -21,5 +38,15 @@ return {
     scroll = { enabled = true },
     statuscolumn = { enabled = false },
     words = { enabled = false },
+    styles = {
+      snacks_image = {
+        relative = "cursor",
+        -- width = 90,
+        -- height = 98,
+        border = true,
+        focusable = false,
+        backdrop = false,
+      },
+    },
   },
 }

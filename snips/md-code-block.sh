82efc5fd8abcf3df3f8dc2640289e3e156d43a3a
Fernando Basso <fernandobasso.br@gmail.com>
2026-01-28 16:35:16 -0300

    misc: Add fenced code block snippet

---
+++ snips/md-code-block.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Add a keyboard shortcut from your WM/DE that simply runs
+# this script, like:
+#
+# /home/yoda/source/dotfiles/snips/md-code-block.sh
+#
+
+cat <<'EOF' | wl-copy --type text/plain
+```
+
+```
+EOF
+

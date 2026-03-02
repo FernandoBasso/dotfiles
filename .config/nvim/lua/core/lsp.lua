5c89db19fc7bd298b3908ec8bc22845fca996b1b
Fernando Basso <fernandobasso.br@gmail.com>
2026-03-02 20:09:04 -0300

    nvim: Disable LSP autocomplete to let blink.cmp do it

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
       vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
       vim.lsp.log.set_level('DEBUG')
 
       -- TODO: C-Space is not triggering completion.

1ac7865c1a328adb6323acdebf7ab926a9a73502
Fernando Basso <fernandobasso.br@gmail.com>
2026-03-30 06:30:41 -0300

    nvim(go): Disable virtual text

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -2,24 +2,32 @@ return {
   "ray-x/go.nvim",
   dependencies = {
     "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
   },
 
   opts = function()
-    require("go").setup(opts)
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    require('go').setup({
+      lsp_cfg = true,
+
+      ----
+      -- Disables things like those “greyed out” types that show
+      -- up for iformational purposes.
+      --
+      lsp_inlay_hints = {
+        enable = false
+      },
+    })
+
+    local format_sync_grp = vim.api.nvim_create_augroup("Format", {
+      clear = false,
+    })
+
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*.go",
       callback = function()
-        require('go.format').goimports()
+        require('go.format').goimport()
       end,
       group = format_sync_grp,
     })
-    return {
-      -- lsp_keymaps = false,
-      -- other options
-    }
   end,
 
   event = {"CmdlineEnter"},

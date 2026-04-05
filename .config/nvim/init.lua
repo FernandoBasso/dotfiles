9fa6b680f793ec8e57b0d22f87058949882ff023
Fernando Basso <fernandobasso.br@gmail.com>
2026-04-05 11:20:27 -0300

    nvim: Add initial vanilla config with vim.pack

---
+++ .config/nvim/init.lua
@@ -0,0 +1,83 @@
+local opt = vim.opt
+local g = vim.g
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local homedir = os.getenv('HOME')
+
+vim.g.mapleader = " "
+
+opt.guicursor = "i:block"
+opt.signcolumn = "yes:1"
+opt.termguicolors = true
+opt.ignorecase = true
+opt.swapfile = true
+opt.autoindent = true
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.shiftround = true
+opt.listchars = "tab:␉ ,trail:·"
+opt.list = true
+opt.number = true
+opt.relativenumber = false
+opt.numberwidth = 2
+opt.wrap = false
+opt.cursorline = true
+opt.scrolloff = 6
+opt.inccommand = "nosplit"
+opt.completeopt = { "menuone", "popup", "noinsert" }
+opt.winborder = "rounded"
+opt.hlsearch = false
+
+vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set laststatus=2")
+
+vim.cmd("set path+=**/*")
+
+opt.backup = true
+opt.backupext = ".vimbkp"
+opt.backupdir = homedir .. "/Temp/vim_bkp/"
+opt.directory = homedir .. "/Temp/vim_tmp//"
+
+vim.cmd.filetype("plugin indent on")
+
+----
+-- Colorscheme
+--
+vim.cmd("colorscheme retrobox")
+
+----
+-- Save with Ctrl+; (I just like it). See `:help update`.
+--
+vim.keymap.set('n', '<C-;>', ':update<CR>')
+vim.keymap.set('i', '<C-;>', '<Esc>:update<CR>')
+
+----
+-- Restore cursor position. :help restore-cursor.
+--
+vim.cmd [[
+  augroup RestoreCursor
+    autocmd!
+    autocmd BufRead * autocmd FileType <buffer> ++once
+      \ let s:line = line("'\"")
+      \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
+      \      && index(['xxd', 'gitrebase'], &filetype) == -1
+      \ |   execute "normal! g`\""
+      \ | endif
+  augroup END
+]]
+
+----
+-- Highlight yanked text.
+--
+local highlight_group = augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({ timeout = 170 })
+    end,
+    group = highlight_group,
+})
+
+require("plugins")
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,270 @@
+local g = vim.g
+-- local keymap = vim.keymap.set
+local silent = { silent = true }
+
+function keymap(mode, keys, action, d)
+  vim.keymap.set(
+    mode,
+    keys,
+    action,
+    {
+      noremap = true,
+      silent = true,
+      nowait = true,
+      desc = d,
+    }
+  )
+end
+
+------------------------------------------------------------------------------
+-- NVIM WEB DEVICONS
+--
+vim.pack.add({
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+})
+
+------------------------------------------------------------------------------
+-- LUALINE
+--
+vim.pack.add({
+  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+})
+
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+  },
+  sections = {
+    lualine_c = {
+      { 'filename', path = 1 }
+    }
+  }
+})
+
+------------------------------------------------------------------------------
+-- NVIM-TREE
+--
+vim.pack.add({
+  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+})
+
+keymap(
+  "n",
+  "<Leader>T",
+  "<cmd>:NvimTreeToggle<CR>",
+  'NVim-Tree: Toggle'
+)
+
+
+require('nvim-tree').setup({
+  update_focused_file = {
+    enable = true,
+  },
+
+  on_attach = function(bufnr)
+    local api = require 'nvim-tree.api'
+
+    local function opts(desc)
+      return {
+        desc = 'NVim-Tree: ' .. desc,
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+
+    ----
+    -- Enable default keymaps.
+    --
+    api.config.mappings.default_on_attach(bufnr)
+
+    ----
+    -- Some useful custom keymaps.
+    --
+    vim.keymap.set(
+      'n',
+      '<C-t>',
+      api.tree.change_root_to_parent,
+      opts('Up')
+    )
+
+    vim.keymap.set(
+      'n',
+      '?',
+      api.tree.toggle_help,
+      opts('Help')
+    )
+  end,
+  filters = {
+    dotfiles = false,
+  },
+  view = {
+    width = {
+      min = 36,
+      max = 42,
+    },
+  },
+})
+
+------------------------------------------------------------------------------
+-- FZF-LUA
+--
+
+vim.pack.add({
+  { src = "https://github.com/ibhagwan/fzf-lua" },
+})
+
+local fzf_lua_actions = require('fzf-lua.actions')
+
+require('fzf-lua').setup({
+  keymap = {
+    builtin = {
+      ["<C-f>"] = "preview-page-down",
+      ["<C-b>"] = "preview-page-up",
+      ["<C-p>"] = "toggle-preview",
+    },
+    fzf = {
+      ["ctrl-a"] = "toggle-all",
+      ["ctrl-t"] = "first",
+      ["ctrl-g"] = "last",
+      ["ctrl-d"] = "half-page-down",
+      ["ctrl-u"] = "half-page-up",
+    }
+  },
+  actions = {
+    files = {
+      ["ctrl-q"] = fzf_lua_actions.file_sel_to_qf,
+      ["ctrl-n"] = fzf_lua_actions.toggle_ignore,
+      ["ctrl-h"] = fzf_lua_actions.toggle_hidden,
+      ["enter"]  = fzf_lua_actions.file_edit_or_qf,
+    }
+  },
+  winopts = {
+    backdrop = 81,
+    width = 0.95,
+    height = 0.95,
+    preview = {
+      layout = "flex",
+    },
+  },
+})
+
+keymap(
+  "n",
+  "<Leader>fn",
+  function() require('fzf-lua').files({ cwd = vim.fn.stdpath("config") }) end,
+  "[F]ind [n]ovim configs"
+)
+keymap(
+  "n",
+  "<Leader><Leader>",
+  function() require('fzf-lua').global() end,
+  --
+  -- https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#global-picker
+  --
+  "LSP: Files, $, @ and #"
+)
+keymap(
+  "n",
+  "<Leader>ff",
+  function() require('fzf-lua').files() end,
+  "[F]ind [f]iles in current project"
+)
+keymap(
+  "n",
+  "<Leader>fb",
+  function() require('fzf-lua').buffers() end,
+  "[F]ind [b]uffers in current project"
+)
+keymap(
+  "n",
+  "<Leader>fg",
+  function() require('fzf-lua').live_grep({ resume = true }) end,
+  "[L]ive [g]rep in current project"
+)
+keymap(
+  "n",
+  "<Leader>fh",
+  function() require('fzf-lua').helptags() end,
+  "[F]ind [h]elp tag"
+)
+keymap(
+  "n",
+  "<Leader>fk",
+  function() require('fzf-lua').keymaps() end,
+  "[F]ind [k]eymaps"
+)
+keymap(
+  "n",
+  "<Leader>f?",
+  function() require('fzf-lua').git_status() end,
+  "[F]ind in git status"
+)
+keymap(
+  "n",
+  "<Leader>fc",
+  function() require('fzf-lua').builtin() end,
+  "[F]find builtin [c]ommand"
+)
+keymap(
+  "n",
+  "<Leader>fr",
+  function() require('fzf-lua').resume() end,
+  "[F]ind [r]esume last search"
+)
+keymap(
+  "n",
+  "<Leader>fw",
+  function() require('fzf-lua').grep_cword() end,
+  "[F]ind c[w]ord"
+)
+keymap(
+  "n",
+  "<Leader>fW",
+  function() require('fzf-lua').grep_cWORD() end,
+  "[F]ind c[W]ORD"
+)
+
+
+------------------------------------------------------------------------
+-- Built-in LSP comes with several default keymaps as of nvim 0.11.
+-- See ../core/lsp.lua. These are some extra keymaps.
+keymap(
+  "n",
+  "<Leader>gd",
+  function() require('fzf-lua').lsp_definitions() end,
+  "[g]o find LSP [d]efinitions"
+)
+keymap(
+  "n",
+  "<Leader>gr",
+  function() require('fzf-lua').lsp_references() end,
+  "[g]o find LSP [r]eferences"
+)
+keymap(
+  "n",
+  "<Leader>gs",
+  function() require('fzf-lua').lsp_document_symbols() end,
+  "[g]o find LSP document [s]symbols"
+)
+keymap(
+  "n",
+  "<Leader>gS",
+  function() require('fzf-lua').lsp_live_workspace_symbols() end,
+  "[g]o find LSP workspace [S]symbols"
+)
+keymap(
+  "n",
+  "<Leader>ld",
+  function() require('fzf-lua').lsp_document_diagnostics() end,
+  "Find [L]SP [d]ocument diagnostics"
+)
+keymap(
+  "n",
+  "<Leader>lD",
+  function() require('fzf-lua').lsp_workspace_diagnostics() end,
+  "Find [L]SP workspace [s]diagnostics"
+)
+

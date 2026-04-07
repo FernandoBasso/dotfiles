bc63882bf9d0bda38da6a5b6d1c9dfa067376018
Fernando Basso <fernandobasso.br@gmail.com>
2026-04-07 08:07:18 -0300

    nvim: Add blink.cmp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -601,6 +601,7 @@ require("luasnip").config.set_config {
   enable_autosnippets = true,
 }
 
+
 --
 -- I actually use digraphs quite often so instead of <C-k> as suggested
 -- in the docs I'll go with <C-j> to insert a snippet or jump forward
@@ -632,4 +633,74 @@ require('luasnip.loaders.from_lua').load({
   paths = {
     '~/.config/nvim/snips/'
   }
+})
+
+------------------------------------------------------------------------------
+-- BLINK CMP
+--
+vim.pack.add({
+  {
+    src = "https://github.com/Saghen/blink.cmp",
+    version = "v1",
+  }
+})
+
+require("blink.cmp").setup({
+  keymap = {
+    ["<C-n>"] = { "select_next", "fallback" },
+    ["<C-p>"] = { "select_prev", "fallback" },
+    ["<CR>"] = { "accept", "fallback" },
+    ["<Tab>"] = {},
+    ["<S-Tab>"] = {},
+    ["<C-f>"] = {},
+    ["<Up>"] = { "select_prev", "fallback" },
+    ["<Down>"] = { "select_next", "fallback" },
+  },
+  cmdline = {
+    enabled = false,
+    completion = { menu = { auto_show = true } },
+    keymap = {
+      ["<CR>"] = { "accept_and_enter", "fallback" },
+    },
+  },
+  completion = {
+    menu = {
+      border = nil,
+      scrolloff = 1,
+      scrollbar = false,
+      draw = {
+        columns = {
+          { "kind_icon" },
+          { "label", "label_description", gap = 1 },
+          { "kind" },
+          { "source_name" },
+        },
+      },
+    },
+    documentation = {
+      window = {
+        border = nil,
+        scrollbar = false,
+        winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+      },
+      auto_show = true,
+      auto_show_delay_ms = 500,
+    },
+  },
+  appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = "mono" },
+  snippets = { preset = "luasnip" },
+  sources = {
+    default = {
+      "lsp",
+      "path",
+      "snippets",
+      "buffer",
+      -- "conjure",
+      -- "ripgrep",
+    },
+    providers = {
+      -- conjure = { name = "conjure", module = "blink.compat.source", score_offset = -3 },
+      -- ripgrep = { module = "blink-ripgrep", name = "Ripgrep", score_offset = -3 },
+    },
+  },
 })

ddb118154911f61a2afd74e0cb0d95f795fb5dac
Fernando Basso <fernandobasso.br@gmail.com>
2026-04-08 20:02:34 -0300

    nvim: Add snacks

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -706,3 +706,57 @@ require("blink.cmp").setup({
     },
   },
 })
+
+------------------------------------------------------------------------------
+-- SNACKS
+--
+vim.pack.add({
+  { src = "https://github.com/folke/snacks.nvim" },
+})
+
+keymap(
+  "n",
+  "<Leader>Ii",
+  function()
+    print("HOVERING")
+    Snacks.image.hover()
+  end,
+  "Show image in float window"
+)
+
+require("snacks").setup({
+  bigfile = { enabled = false },
+  dashboard = { enabled = false },
+  explorer = { enabled = false },
+  image = {
+    enabled = true,
+    inline = false,
+    float = true,
+    max_width = 100,
+    max_height = 100,
+  },
+  indent = { enabled = true },
+  input = {
+    enabled = true,
+    win = {
+      relative = "cursor",
+    }
+  },
+  picker = { enabled = false },
+  notifier = { enabled = true },
+  quickfile = { enabled = false },
+  scope = { enabled = true },
+  scroll = { enabled = true },
+  statuscolumn = { enabled = false },
+  words = { enabled = false },
+  styles = {
+    snacks_image = {
+      relative = "cursor",
+      -- width = 90,
+      -- height = 98,
+      border = true,
+      focusable = false,
+      backdrop = false,
+    },
+  },
+})

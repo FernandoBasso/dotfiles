54790b9a1e33c99b609e1d18029986f627c86ed7
Fernando Basso <fernandobasso.br@gmail.com>
2026-08-20 09:56:22 -0300

    nvim: Add img-clip with initial config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -121,6 +121,7 @@ vim.pack.add({
   "https://github.com/ray-x/guihua.lua",
   "https://github.com/Bekaboo/dropbar.nvim",
   "https://github.com/L3MON4D3/LuaSnip",
+  "https://github.com/hakonharnes/img-clip.nvim",
 
   {
     src = "https://github.com/Saghen/blink.cmp",
@@ -863,6 +864,72 @@ require("snacks").setup({
       border = true,
       focusable = false,
       backdrop = false,
+    },
+  },
+})
+
+------------------------------------------------------------------------------
+-- IMG-CLIP
+--
+local imgclip = require("img-clip")
+
+imgclip.setup({
+  relative_to_current_file = true,
+
+  filetypes = {
+    asciidoc = {
+      ----
+      -- Make the image directory location relative to the location of
+      -- the current buffer. For example, if we have:
+      --
+      --   ~/source/myproj/docs/cmdline/sed-intro.adoc
+      --
+      -- The, the image is place under:
+      --
+      --   ~/source/myproj/docs/cmdline/__assets/<image-name>.png
+      --
+      -- For AsciiDoc[tor], I like to always keep the images under an
+      -- `__assets/` directory relative to the current file.
+      --
+      dir_path = function()
+        return vim.fn.expand('%:.:h') .. '/__assets'
+      end,
+
+      url_encode_path = true, ---@type boolean | fun(): boolean
+
+      ----
+      -- • https://docs.asciidoctor.org/asciidoc/latest/macros/images/
+      --
+      template = "image::$FILE_PATH[$CURSOR]", ---@type string | fun(context: table): string
+
+      download_images = false, ---@type boolean | fun(): boolean
+    },
+    markdown = {
+      ----
+      -- Make the image directory location relative to the location of
+      -- the current buffer. For example, if we have:
+      --
+      --   ~/source/myproj/docs/cmdline/sed-intro.adoc
+      --
+      -- The, the image is place under:
+      --
+      --   ~/source/myproj/docs/cmdline/__assets/<image-name>.png
+      --
+      -- For AsciiDoc[tor], I like to always keep the images under an
+      -- `__assets/` directory relative to the current file.
+      --
+      dir_path = function()
+        return vim.fn.expand('%:.:h') .. '/__assets'
+      end,
+
+      url_encode_path = true, ---@type boolean | fun(): boolean
+
+      ----
+      -- • https://docs.asciidoctor.org/asciidoc/latest/macros/images/
+      --
+      template = "![$CURSOR]($FILE_PATH)", ---@type string | fun(context: table): string
+
+      download_images = false, ---@type boolean | fun(): boolean
     },
   },
 })
